ab81876ae9c4b9a04ec662571e1d182783fb86e9
Jerit-Baiju <jeritalumkal@gmail.com>
2022-05-08 16:36:45 +0530

    .

--- main.py
+++ main.py
@@ -7,15 +7,28 @@ r = random.randint(a,b)
 print(f'random num = {r}')
 
 for x in range(10):
-    if r == (a+b)/2:
+    c = (a+b)/2
+    if c % 2 != 0:
+        if p == 'more':
+            c = c - 1
+        elif p == 'less':
+            c = c + 1
+    print(f'c is {c}')
+    if r == c:
         print('win')
+        break
 
-    elif r < (a+b)/2:
-        print('r is less')
-        c = (a+b)/2
-
-    elif r > (a+b)/2:
+    elif r > c:
+        p = 'more'
         print('r is more')
-        c = (a+b)/2
+        a = c
+        
+    elif r < c:
+        p = 'less'
+        print('r is less')
+        b = c
+            
     else:
         print('else')
+
+print(r)
